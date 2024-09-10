26ac85b3d4dad78d695c91623f456f39cab63a06
TomasRodriguez30 <rodrigueztomas.dev@gmail.com>
2024-09-09 22:11:07 -0300

    TP1 - Parte 1

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const cards = document.querySelectorAll('.card-cursos, .card-estudiantes, .card-ui');
+    const cards = document.querySelectorAll('.persona, .card-cursos, .card-estudiantes, .card-ui');
     cards.forEach((card) => {
         card.classList.add('animate');
     });
