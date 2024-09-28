7d2b6a004510a9d01c6739f34a3fa0900d34e93c
TomasRodriguez30 <rodrigueztomas.dev@gmail.com>
2024-09-28 01:30:37 -0300

    Parte 1: Migracion a VueJS

--- vue-login/src/views/HomeView.vue
+++ vue-login/src/views/HomeView.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore();
 </script>
 
 <template>
     <div>
         <h1>Bienvenido a la página de inicio</h1>
+        <div v-if="authStore.isAuthenticated">
+            <h2>Datos del usuario:</h2>
+            <p>Nombre de usuario: {{ authStore.user }}</p>
+        </div>
+        <div v-else>
+            <p>No hay usuario autenticado.</p>
+        </div>
     </div>
 </template>
 
 <style scoped>
-    h1 {
+    h1, h2, p {
         color: #fff;
     }
 </style>
